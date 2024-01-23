2df5daaf9c91b4ca3d63b2acfb718dff1876bd08
Smartisek <domalipdominik@gmail.com>
2024-01-23 19:25:23 +0000

    creating stacks

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -1,2 +1,19 @@
+import java.util.Stack;
+
+/**
+ *  Name: Dominik Domalip
+ *  Class Group: GD2b
+ */
+
 public class CA3_Question1 {
+
+    public static void runSimulation(){
+        Stack<Integer> driveway = new Stack<Integer>();
+        Stack<Integer> street = new Stack<Integer>();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
