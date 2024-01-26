6b44956ae286b44cb6f3967e82bea47eb887f297
Smartisek <domalipdominik@gmail.com>
2024-01-26 20:38:11 +0000

    adding some ui

--- src/CA3_Question2.java
+++ src/CA3_Question2.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- *  Name:
- *  Class Group:
+ *  Name: Dominik Domalip
+ *  Class Group: GD2b
  */
 public class CA3_Question2
 {
@@ -12,7 +12,6 @@ public class CA3_Question2
 
      */
     public static int[][]  floodFillStart() {
-        Scanner kb = new Scanner(System.in);
         int[][] arr = new int[10][10];
         for (int x = 0; x < 10; x++)
         {
@@ -21,9 +20,12 @@ public class CA3_Question2
                 arr[x][y] = 0;
             }
         }
+        arr[0][1] = -1;
+        arr[1][0] = -1;
         return arr;
     }
 
+    // creating a class that behaves as a pair
     static class Cell{
         int x;
         int y;
@@ -57,33 +59,38 @@ public class CA3_Question2
     }
     private static void fill(int r, int c, int[][] arr)
     {
+//        creating a stack of a cellPair with parameter cell class to be able to push coordinates into it
         Stack <Cell> cellPair = new Stack<Cell>();
+//       pushing onto this stack a new cell r,c, parameter of a function that will be chosen once function called
         cellPair.push(new Cell(r, c));
-        int count =1;
-
+        int count =1; // count for displaying the journey of the fill
+//      while this stack is not empty do this
         while(!cellPair.empty()){
+//            create a variable for popping a stack
             Cell current = cellPair.pop();
+//            getting x and y of popped stack
             int x = current.getX();
             int y = current.getY();
+//            incrementing count and assigning it to array
             arr[x][y] = count;
             count++;
-//check if it has already been filled
-            //check north
+
+            //check north if cell is 0
             if(x>0 && arr[x-1][y] == 0) {
-                arr[x-1][y] = count;
-                cellPair.push(new Cell(x-1, y));
+                arr[x-1][y] = count; //set current position to current incrementation number of count
+                cellPair.push(new Cell(x-1, y)); //push this cell onto the stack meaning stack is not empty and process of checking will continue
             }
-            //check west
+            //check west if cell is 0
             if(y>0 && arr[x][y-1] == 0){
                 arr[x][y-1] = count;
                 cellPair.push(new Cell(x, y-1));
             }
-            //check south
+            //check south if cell is 0
             if(x < 9 && arr[x+1][y] == 0){
                 arr[x+1][y] = count;
                 cellPair.push(new Cell(x+1, y));
             }
-            //check east
+            //check east if cell is 0
             if(y<9 && arr[x][y+1] == 0){
                 arr[x][y+1] = count;
                 cellPair.push(new Cell(x, y+1));
@@ -94,8 +101,14 @@ public class CA3_Question2
 
     public static void start()
     {
-        int[][] arr = floodFillStart();
-        fill(2,5, arr);
+        int[][] arr = floodFillStart(); //initialize the array
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose x position: ");
+        int x = sc.nextInt();
+        System.out.println("Choose y position: ");
+        int y = sc.nextInt();
+        //start floodFill algorithm
+        fill(x,y, arr);
 
         display(arr);
 
