3f0cb25c09b65ec9dc01072cee6967b6cd1f5960
Smartisek <domalipdominik@gmail.com>
2024-01-31 19:23:42 +0000

    adding explanations

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,11 +1,12 @@
-import java.util.LinkedList;
-import java.util.Queue;
+
 import java.util.Scanner;
 
 /**
  *  Name: Dominik Domalip
  *  Class Group: GD2b
  */
+
+// References: https://stackoverflow.com/questions/19950713/scanner-input-validation-in-while-loop for continuous user input
 
 public class CA3_Question5
 {
@@ -15,20 +16,24 @@ public class CA3_Question5
     }
 
     public static void commandLine(){
+//        initialize the class for being able to access its functions and the queues
         CA3_Question5AirportClass airport = new CA3_Question5AirportClass();
 
-//        Queue<String> takeoff = new LinkedList<String>();
-//        Queue<String> land = new LinkedList<String>();
-
+//      creating a new scanner for input
         Scanner scanner = new Scanner(System.in);
+
+//        handling the users input while true make a new scanner line, goes false when requested quit
         while(true){
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine(); //creating new scanner line each time
             if(userInput.startsWith("takeoff")){
+//                checking if the input starts with a word takeoff and if yes make a string variable that has that input split with space
+//                and adding second word, first index to that variable meaning the flight number
                 String flightNum = userInput.split(" ")[1];
                 airport.takeoff(flightNum);
             } else if(userInput.startsWith("land")){
                 String flightNum = userInput.split(" ")[1];
                 airport.land(flightNum);
+//                calling the function handling when requested next
             } else if(userInput.startsWith("next")){
                 airport.flightOrganizator();
             } else if(userInput.startsWith("quit")){
--- src/CA3_Question5AirportClass.java
+++ src/CA3_Question5AirportClass.java
@@ -2,36 +2,45 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class CA3_Question5AirportClass {
+//    Creating initial queues
     private Queue<String> takeoff;
     private Queue<String> land;
 
+//    making my queues to be a linked in like in regular initialization of a queue
     public CA3_Question5AirportClass(){
         takeoff = new LinkedList<String>();
         land = new LinkedList<String>();
     }
 
+//    function for when takeoff is requested and adding that flight into the takeoff queue
     public void takeoff(String flightNum){
         takeoff.add(flightNum);
         System.out.println("Flight " + flightNum + " was added to the takeoff queue");
     }
 
+//    function for requesting landing and adding that flight into the land queue
     public void land(String flightNum){
         land.add(flightNum);
         System.out.println("Flight " + flightNum + " was added to the land queue");
     }
 
+//    function for polling from the queues
     public void flightOrganizator(){
+//        with priority to landing, we check if landing queue is not empty first and if it is true then we poll that flight from the queue
         if(!land.isEmpty()){
             System.out.println("Flight " + land.peek() + " has landed.");
             land.poll();
+//            if all requested landings were finished then we check if there are any takeoffs and poll each flight when requesting next in command line
         } else if(!takeoff.isEmpty()){
             System.out.println("Flight " + takeoff.peek() + " has taken off.");
             takeoff.poll();
+//            if in the command line is a request for next but there are not any flights then print out a message
         } else if(land.isEmpty() && takeoff.isEmpty()){
             System.out.println("There are no departing or landing flights right now.");
         }
     }
 
+//    function with message for when requested quit
     public void quitSimulation(){
         System.out.println("The simulation has ended.");
     }
