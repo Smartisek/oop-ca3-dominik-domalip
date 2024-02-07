1f66247061aecf2c44319b6179f4139034059673
Smartisek <domalipdominik@gmail.com>
2024-02-07 17:31:50 +0000

    creating a class for question 7

--- src/CA3_Question7SharesClass.java
+++ src/CA3_Question7SharesClass.java
@@ -1,3 +1,36 @@
-public class CA3_Question7SharesClass {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+    class Pie{
+        private int quantity;
+        private double price;
+
+        //    Block constructor
+        public Pie(int quantity, double price){
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public int getQuantity(){return quantity;}
+        public int setQuantity(int quantityNew){return this.quantity = quantityNew;}
+
+        public double getPrice(){return price;}
+    }
+
+public class CA3_Question7SharesClass{
+        Map<String, Queue<Pie>> stock;
+        public CA3_Question7SharesClass() {
+            this.stock = new HashMap<>();
+        }
+
+        public void buy(String company, int quantity, double price){
+            Pie pie = new Pie(quantity, price);
+            if(!stock.containsKey(company)){
+                stock.put(company, new LinkedList<>());
+            }
+            stock.get(company).add(pie);
+        }
 
 }
