7334340992efd51ce4f3d87f2879d7d38e072b28
Smartisek <domalipdominik@gmail.com>
2024-02-08 18:26:20 +0000

    adding sample code for question 8

---
+++ src/CA3_Question8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ *  Name: Dominik Domalip
+ *  Class Group: GD2b
+ */
+public class CA3_Question8 {
+
+    /*
+        Reads in an equation from the user
+     */
+    public static void main(String[] args) {
+        String equation;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter equation");
+        equation = in.nextLine().trim();
+        System.out.println(equation);
+    }
+}
