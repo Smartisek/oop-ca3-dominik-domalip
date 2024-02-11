026bf3370a8b469b4df88771d43d709ddf4647e6
Smartisek <domalipdominik@gmail.com>
2024-02-11 16:51:15 +0000

    implementing code with recursion

--- src/CA3_Question9.java
+++ src/CA3_Question9.java
@@ -39,15 +39,60 @@ public class CA3_Question9
             System.out.println();
         }
     }
-    public void solve(int x, int y, DIRECTION dir)
+    public static boolean solve(int x, int y, int [][] arr,Stack<CA3_Question2.Cell> path)
     {
-        Stack<DIRECTION> directions = new Stack<>();
+        CA3_Question2.Cell start = new CA3_Question2.Cell(x, y);
+        path.push(start);
 
+        if(x < 0 || y < 0 || x >= arr.length || y >= arr[0].length){
+            return false;
+        }
+
+        if(arr[x][y] == 7){
+            path.push((new CA3_Question2.Cell(x,y)));
+            return true;
+        }
+
+        if(arr[x][y] != 0){
+            return false;
+        }
+
+        arr[x][y] = -1;
+
+
+        for(DIRECTION direction : DIRECTION.values()){
+            int newX = x + direction.getX();
+            int newY = y + direction.getY();
+            if(solve(newX, newY, arr, path)){
+                return true;
+            }
+        }
+        path.pop();
+        return false;
     }
 
     public static void main(String[] args) {
-        int[][] image = new int[][]{{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 0, 0, 1}, {-99, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+        Stack<CA3_Question2.Cell> path = new Stack<>();
+        int[][] image = new int[][]{
+                {1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 0, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 1},
+                {1, 1, 7, 1, 1}
+        };
+        display(image);
+
+
+        if(solve(3, 1, image, path)){
+            System.out.println("Path found buttom up: ");
+            while(!path.isEmpty()){
+                CA3_Question2.Cell cell = path.pop();
+                System.out.println("( " + cell.getX() + ", " + cell.getY() + ")");
+            }
+        } else {
+            System.out.println("No path was found");
+        }
         display(image);
     }
-
 }
